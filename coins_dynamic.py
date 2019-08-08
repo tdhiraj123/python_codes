c193fe652281710bf9b3bb0e51260687f42b24f0
tdhiraj123 <45971445+tdhiraj123@users.noreply.github.com>
2019-08-08 15:02:07 +0530

    Add files via upload

---
+++ coins_dynamic.py
@@ -0,0 +1,24 @@
+amt = int(input('amount: '))
+coin =list(map(int,input().split()))
+
+dp=[[0 for _ in range(amt+1)] for _ in range(len(coin)+1)]
+
+dp[0][0]=1
+for i in range(1,len(coin)+1):
+
+    dp[i][0]=1
+
+    for j in range(1,amt+1):
+        cv=coin[i-1]
+
+        woc=dp[i-1][j]
+        wc=0
+        if(cv<=j):
+            wc=dp[i][j-cv]
+        dp[i][j]=woc+wc
+
+print(dp)
+        
+'''
+https://github.com/bephrem1/backtobackswe/blob/master/Dynamic%20Programming%2C%20Recursion%2C%20%26%20Backtracking/changeMakingProblem2.java
+'''
